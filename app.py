00295e4af636586692619e99c4e014011a39f5ee
Milad Younis <milad.younis@sap.com>
2024-07-17 09:37:30 +0300

    changing pp

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_files = current_dir / "assets" / "CV - MiladYounis.pdf"
-profile_pic = current_dir / "assets" / "picofme.png"
+profile_pic = current_dir / "assets" / "profile-pic.png"
 
 PAGE_TITLE = "Data Nerd | Milad Younis"
 PAGE_ICON = ":wave:"
