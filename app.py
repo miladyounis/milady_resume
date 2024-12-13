717c97705fa539c20bfd31e6b3bfb279749ab014
Milad Younis <milad.younis@sap.com>
2024-12-13 12:22:33 +0200

    changed CV

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_files = current_dir / "assets" / "CV - MiladYounis.pdf"
+resume_files = current_dir / "assets" / "MiladYounisResume.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 PAGE_TITLE = "Data Nerd | Milad Younis"
